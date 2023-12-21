990a46a3e1bce2db4bc5bcc40ad171cd755e796a
Victor Fernandes <90485435+VictorFMag@users.noreply.github.com>
2023-12-21 09:53:57 -0300

    Add files via upload

---
+++ Ex2/servidor-imagens.js
@@ -0,0 +1,30 @@
+port = 3000
+
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use('/imagens', express.static('./imagens'))
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado! Porta: ${port}`)
+})
+
+const arrayImagens = fs.readdirSync("./imagens")
+let indice = 0;
+
+app.get('/imagens', (req, res, next) => {
+    if (indice>arrayImagens.length-1){
+        indice = 0
+    } 
+    const enderecoImagem = arrayImagens[indice]
+    const enderecoCompleto = "http://localhost:3000/imagens/"+enderecoImagem
+    indice++
+    console.log("indice: "+indice)
+   res.send({enderecoImagem:enderecoCompleto})
+
+});
